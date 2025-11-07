131a50e32bf6335be989331f6c9ab805a9de3331
Pavitra <pavitraxsingh@gmail.com>
2025-11-07 10:51:09 +0530

    Add Day55_Find Majority Element in an Array.c

--- Day55_Find Majority Element in an Array.c
+++ Day55_Find Majority Element in an Array.c
@@ -1,4 +1,6 @@
-//Q105: Write a program to take an integer array nums of size n, and print the majority element. The majority element is the element that appears strictly more than ⌊n / 2⌋ times. Print -1 if no such element exists. Note: Majority Element is not necessarily the element that is present most number of times.
+//Q105: Write a program to take an integer array nums of size n, and print the majority element. 
+//The majority element is the element that appears strictly more than ⌊n / 2⌋ times. Print -1 if no such element exists.
+//Note: Majority Element is not necessarily the element that is present most number of times.
 
 #include <stdio.h>
 
@@ -26,3 +28,4 @@ int main() {
     printf("%d", majority);
     return 0;
 }
+
