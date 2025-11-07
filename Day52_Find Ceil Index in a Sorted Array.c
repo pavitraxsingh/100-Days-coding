1ba17058d7e0d19dc08f5801829a493180a18156
Pavitra <pavitraxsingh@gmail.com>
2025-11-07 10:43:37 +0530

    Rename file to 'Day52_Find Ceil Index in a Sorted Array.c'

--- Day52_Find Ceil Index in a Sorted Array.c
+++ Day52_Find Ceil Index in a Sorted Array.c
@@ -1,4 +1,5 @@
-//Q102: Write a Program to take a sorted array arr[] and an integer x as input, find the index (0-based) of the smallest element in arr[] that is greater than or equal to x and print it. This element is called the ceil of x. If such an element does not exist, print -1. Note: In case of multiple occurrences of ceil of x, return the index of the first occurrence.
+//Q102: Write a Program to take a sorted array arr[] and an integer x as input, find the index (0-based) of the smallest element in arr[] that is greater than or equal to x and print it. This element is called the ceil of x.
+//If such an element does not exist, print -1. Note: In case of multiple occurrences of ceil of x, return the index of the first occurrence.
 
 #include <stdio.h>
 
@@ -22,3 +23,4 @@ int main() {
     printf("%d", index);
     return 0;
 }
+
