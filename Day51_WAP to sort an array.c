349cb979deb4482de402971c6beb9699a585fa9e
Pavitra <pavitraxsingh@gmail.com>
2025-11-07 10:40:56 +0530

    Rename file to Day51_WAP to sort an array.c

--- Day51_WAP to sort an array.c
+++ Day51_WAP to sort an array.c
@@ -1,4 +1,5 @@
-//Q101: Write a Program to take a sorted array(say nums[]) and an integer (say target) as inputs. The elements in the sorted array might be repeated. You need to print the first and last occurrence of the target and print the index of first and last occurrence. Print -1, -1 if the target is not present.
+//Q101: Write a Program to take a sorted array(say nums[]) and an integer (say target) as inputs. The elements in the sorted array might be repeated. 
+//You need to print the first and last occurrence of the target and print the index of first and last occurrence. Print -1, -1 if the target is not present.
 
 #include <stdio.h>
 
@@ -23,3 +24,4 @@ int main() {
     printf("%d,%d", first, last);
     return 0;
 }
+
